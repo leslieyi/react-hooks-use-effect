f42393987b404030db213b15506b7b7f2d76570c
leslieyi <leslieyi411@gmail.com>
2021-07-27 10:40:09 -0400

    Not sure how to reset every 5 seconds?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from "react";
 
+
 function App() {
+  const [isCounted, setIsCounted] = useState(0)
+  const [isTyped, setIsTyped] = useState("")
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = isTyped;
+  }, [isTyped]);
+
+  useEffect(() => { 
+    setTimeout(() => setIsCounted(0,  console.log("I will appear after 5 seconds")), 5000);
+   
+  },[]);
 
   console.log("Component rendering");
+ 
 
-  return <button>Click Me</button>;
+  function handleClick(){
+    setIsCounted(isCounted => isCounted +1)
+  }
+
+  function handleType(e){
+    setIsTyped(e.target.value)
+    console.log(e.target.value)
+
+  }
+
+
+  return (
+    <div>
+      <button onClick={handleClick}>I've been clicked{isCounted} times</button>
+      <input onChange={handleType} type="text" placeholder="Type away..." value={isTyped} />
+    </div>
+  )
 }
 
 export default App;
